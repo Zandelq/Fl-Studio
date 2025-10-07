54be071e6ad984b65debe1d0aeacf4e8846e1601
Zandelq <133067605+Zandelq@users.noreply.github.com>
2025-10-07 18:36:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, send_from_directory
-import os
 
 app = Flask(__name__)
 
-# Serve the homepage
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Serve sounds
 @app.route('/sounds/<path:filename>')
 def sounds(filename):
     return send_from_directory('sounds', filename)
